3622c714fa273a055149dd9337fd8f79f3d3b565
snowsoft <abdulkadirkar@gmail.com>
2022-05-19 22:51:45 +0300

    -
    -

--- src/Editor.php
+++ src/Editor.php
@@ -6,20 +6,34 @@ use Encore\Admin\Form\Field\Textarea;
 
 class Editor extends Textarea
 {
-    protected $view = 'laravel-admin-ckeditor::editor';
+    protected $view = 'laravel-admin-tinymce::editor';
 
     protected static $js = [
-        'vendor/laravel-admin-ext/ckeditor/ckeditor.js',
+        'vendor/snowsoft/tinymce6/tinymce.min.js',
     ];
 
     public function render()
     {
-        $config = (array) CKEditor::config('config');
+        $config = (array) Tinymce::config('config');
 
         $config = json_encode(array_merge($config, $this->options));
 
         $this->script = <<<EOT
-CKEDITOR.replace('{$this->id}', $config);
+        tinymce.init({
+            selector: 'textarea#{$this->id}',
+            height: 500,
+            plugins: [
+              'advlist', 'autolink', 'lists', 'link', 'image', 'charmap', 'preview',
+              'anchor', 'searchreplace', 'visualblocks', 'code', 'fullscreen',
+              'insertdatetime', 'media', 'table', 'help', 'wordcount'
+            ],
+            toolbar: 'undo redo | blocks | ' +
+            'bold italic backcolor | alignleft aligncenter ' +
+            'alignright alignjustify | bullist numlist outdent indent | ' +
+            'removeformat | help',
+            content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:16px }'
+          });
+  
 EOT;
         return parent::render();
     }
---
+++ src/TinymceServiceProvider.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Encore\Tinymce;
+
+use Encore\Admin\Admin;
+use Encore\Admin\Form;
+use Illuminate\Support\ServiceProvider;
+
+class TinymceServiceProvider extends ServiceProvider
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function boot(Tinymce $extension)
+    {
+        if (! Tinymce::boot()) {
+            return ;
+        }
+
+        if ($views = $extension->views()) {
+            $this->loadViewsFrom($views, 'laravel-admin-tinymce');
+        }
+
+        if ($this->app->runningInConsole() && $assets = $extension->assets()) {
+            $this->publishes(
+                [$assets => public_path('vendor/snowsoft/tinymce')],
+                'laravel-admin-tinymce'
+            );
+        }
+
+        Admin::booting(function () {
+            Form::extend('tinymce', Editor::class);
+        });
+    }
+}
